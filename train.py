5a09fa6fd1eb557d76a1de3b404000e7b108f0ab
Ayews <ayews233@aol.com>
2023-06-06 13:43:31 +0800

    bce+iou

--- train.py
+++ train.py
@@ -41,17 +41,22 @@ def train_one_epoch(epoch,epochs,model,opt,scheduler,train_dl,train_size):
 
         mask_1_4 = F.interpolate(mask_1_4,(H,W),mode='bilinear')
         mask_1_2 = F.interpolate(mask_1_2,(H,W),mode='bilinear')
+        mask_1_1 = F.interpolate(mask_1_1,(H,W),mode='bilinear')
         
         unc_1_4 = F.interpolate(unc_1_4,(H,W),mode='bilinear')
         unc_1_2 = F.interpolate(unc_1_2,(H,W),mode='bilinear')
+        unc_1_1 = F.interpolate(unc_1_1,(H,W),mode='bilinear')
 
         loss4 = F.binary_cross_entropy_with_logits(sal_1_16, label) + iou_loss(sal_1_16, label)
         loss3 = F.binary_cross_entropy_with_logits(sal_1_8, label) + iou_loss(sal_1_8, label)
         loss2_ = F.binary_cross_entropy_with_logits(sal_1_4, label) + iou_loss(sal_1_4, label)
-        loss2 = structure_loss(mask_1_4,label,unc_1_4)#wbce(mask_1_4, label, unc_1_4) + iou_loss(mask_1_4, label)
-        loss1 = structure_loss(mask_1_2, label, unc_1_2)#wbce(mask_1_2, label, unc_1_2) + iou_loss(mask_1_2, label)
-        loss0 = structure_loss(mask_1_1, label, unc_1_1)#wbce(mask_1_1, label, unc_1_1) + iou_loss(mask_1_1, label)
-
+        #loss2 = structure_loss(mask_1_4,label,unc_1_4)#wbce(mask_1_4, label, unc_1_4) + iou_loss(mask_1_4, label)
+        #loss1 = structure_loss(mask_1_2, label, unc_1_2)#wbce(mask_1_2, label, unc_1_2) + iou_loss(mask_1_2, label)
+        #loss0 = structure_loss(mask_1_1, label, unc_1_1)#wbce(mask_1_1, label, unc_1_1) + iou_loss(mask_1_1, label)
+        loss2 = F.binary_cross_entropy_with_logits(mask_1_4, label) + iou_loss(mask_1_4, label)
+        loss1 = F.binary_cross_entropy_with_logits(mask_1_2, label) + iou_loss(mask_1_2, label)
+        loss0 = F.binary_cross_entropy_with_logits(mask_1_1, label) + iou_loss(mask_1_1, label)
+        
         loss = loss_weights[0] * loss0 + loss_weights[1] * loss1 + loss_weights[2] * loss2 + loss_weights[2] * loss2_ + loss_weights[3] * loss3 + loss_weights[4] * loss4
 
         scloss3 = consistent_loss(sal_1_8,sal_1_16.detach()) * 0.0001
