8d88bed679bcf7d1aef125257ab5a86be8229cf2
Ayews <ayews233@aol.com>
2023-06-20 18:40:34 +0800

    update+nn.mhsa+mask

--- lib/uncertainty_aware_refine_attention.py
+++ lib/uncertainty_aware_refine_attention.py
@@ -21,49 +21,20 @@ class URA(nn.Module):
         self.depth = depth
         self.window_size = base_size[0]//8
         self.channel_trans = Conv2d(c_num,depth,1)
-        self.pthreshold = 5
+        self.pthreshold = 0.1
         self.norm = nn.BatchNorm2d(depth)
         self.lnorm = nn.BatchNorm2d(depth)
-        
-        k = cv2.getGaussianKernel(7, 1)
-        k = np.outer(k, k)
-        k = torch.tensor(k).float()
-        self.kernel = k.repeat(1, 1, 1, 1).cuda()
 
+        self.mha = nn.MultiheadAttention(depth,1,batch_first=True)
+        self.q = nn.Linear(depth,depth)
+        self.k = nn.Linear(depth,depth)
+        self.v = nn.Linear(depth,depth)
 
-        # define a parameter table of relative position bias
-        self.relative_position_bias_table = nn.Parameter(
-            torch.zeros((2 * window_size - 1) * (2 * window_size - 1), 1))  # 2*Wh-1 * 2*Ww-1, nH
-
-        # get pair-wise relative position index for each token inside the window
-        coords_h = torch.arange(self.window_size)
-        coords_w = torch.arange(self.window_size)
-        coords = torch.stack(torch.meshgrid([coords_h, coords_w]))  # 2, Wh, Ww
-        coords_flatten = torch.flatten(coords, 1)  # 2, Wh*Ww
-        relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]  # 2, Wh*Ww, Wh*Ww
-        relative_coords = relative_coords.permute(1, 2, 0).contiguous()  # Wh*Ww, Wh*Ww, 2
-        relative_coords[:, :, 0] += self.window_size - 1  # shift to start from 0
-        relative_coords[:, :, 1] += self.window_size - 1
-        relative_coords[:, :, 0] *= 2 * self.window_size - 1
-        relative_position_index = relative_coords.sum(-1)  # Wh*Ww, Wh*Ww
-        self.register_buffer("relative_position_index", relative_position_index)
-
-        self.q = nn.Sequential(
-            nn.Linear(depth,depth)
-        )
-        self.k = nn.Sequential(
-            nn.Linear(depth,depth)
-        )
-        self.v = nn.Sequential(
-            nn.Linear(depth,depth)
-        )
-
-        self.conv_out1 = Conv2d(depth,depth,3,relu=True)
-        
+        self.conv_out1 = nn.Linear(depth,depth)
         self.conv_out3 = Conv2d(depth, depth, 3, relu=True)
         self.conv_out4 = Conv2d(depth, out_channel, 1)
 
-        self.forward = self.__forward
+        self.forward = self._forward
 
         self.ptime = 0.0
         self.rtime = 0.0
@@ -92,23 +63,26 @@ class URA(nn.Module):
         for i in range(0,4):
             #p = np.random.rand()
             #print(p)
-            p = torch.sum(u_w[i]*100)/(h*w)
+            p = torch.sum(u_w[i])/(h*w)
             #print(p)
             if (p<self.pthreshold and h > 24) or h > 48: # partition or not
                 #x_w[i],p_w[i] = self.DWPA(x_w[i],l_w[i],u_w[i],p_w[i])
                 x_w[i] = self.DWPA(x_w[i],l_w[i],u_w[i])
             else:
                 st = time.process_time()
-                q= self.q(x_w[i].flatten(-2).transpose(-1,-2))
-                k = self.k(l_w[i].flatten(-2).transpose(-1,-2))
-                v = self.v(l_w[i].flatten(-2).transpose(-1,-2))
+                q = x_w[i].flatten(-2).transpose(-1,-2)
+                k = l_w[i].flatten(-2).transpose(-1,-2)
+                v = l_w[i].flatten(-2).transpose(-1,-2)
                 u = u_w[i].flatten(-2).transpose(-1,-2) 
-                attn = q @ k.transpose(-2,-1)
-                attn = (self.depth ** -.5) * attn
-                uattn = u @ u.transpose(-2,-1)
-                attn=attn+uattn/uattn.max()*attn.max()
-                attn = (attn @ v).transpose(-2,-1).view(B, C, h, w)
-                attn = self.conv_out1(attn)
+                umask = u @ u.transpose(-1,-2)           
+                attn_mask = (umask>self.pthreshold).bool()
+                new_attn_mask = torch.zeros_like(attn_mask, dtype=q.dtype)
+                new_attn_mask.masked_fill_(attn_mask, float("-1e10"))
+                attn,attnw = self.mha(q,k,v,attn_mask = new_attn_mask)
+                attn=torch.where(torch.isnan(attn),torch.full_like(attn,0),attn)
+                #print(attn)
+                attn = self.conv_out1(attn).transpose(-2,-1).view(B, C, h, w)
+                #print(attn)
                 x_w[i] += attn
                 et = time.process_time()
                 self.etime += (et-st)
@@ -119,52 +93,9 @@ class URA(nn.Module):
         self.rtime+=(et-st)
         return x_w
 
-    def _forward(self, x, l, map_s,map_l=None):
-        
-        B,C,H,W = x.shape
-        cg = self.get_uncertain(map_s,(H,W))
 
-        x_uncertain = l
-        
-        st = time.process_time()
-        #cg_windows = window_partition(cg,self.window_size).flatten(2).transpose(1,2)
-        x_windows = window_partition(self.norm(x),self.window_size).flatten(2).transpose(1,2)
-        c_windows = window_partition(self.norm(x_uncertain),self.window_size).flatten(2).transpose(1,2)
-        et = time.process_time()
-        self.ptime+=(et-st)
-        st = time.process_time()
-        b = x_windows.shape[0]
-        q = self.q(x_windows)
-        k = self.k(c_windows)
-        v = self.v(c_windows)
-        attn = q @ k.transpose(-2, -1)
-        attn = (self.depth ** -.5) * attn
-
-        #cg_ = cg_windows @ cg_windows.transpose(1,2)+0.5
-        #attn=attn+torch.log(cg_)
-        #relative_position_bias = self.relative_position_bias_table[self.relative_position_index.view(-1)].view(
-        #    self.window_size * self.window_size, self.window_size * self.window_size, -1)  # Wh*Ww,Wh*Ww,nH
-        #relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous()  # nH, Wh*Ww, Wh*Ww
-        #attn = attn + relative_position_bias#.unsqueeze(0)
-
-        attn = F.softmax(attn, dim=-1)
-        attn = (attn @ v).view(b, self.window_size, self.window_size, C)
-        et = time.process_time()
-        self.etime+=(et-st)        
-        st = time.process_time()
-        x_reverse = window_reverse(attn,self.window_size,H,W)
-        et = time.process_time()
-        self.rtime+=(et-st)
-        x_reverse = self.conv_out1(x_reverse)
-        x = x+x_reverse
-        
-        #x = self.conv_out2(x)
-        x = self.conv_out3(x)
-        out = self.conv_out4(x)
-
-        return x, out, cg
     
-    def __forward(self,x,l,umap):
+    def _forward(self,x,l,umap):
                 
         B,C,H,W = x.shape
         #umap = self.get_uncertain(smap,(H,W))
