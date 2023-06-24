aa3bb490358719ee2ae4efdd389d6147d8853650
Ayews <ayews233@aol.com>
2023-06-24 22:47:14 +0800

    -sc

--- train.py
+++ train.py
@@ -50,19 +50,19 @@ def train_one_epoch(epoch,epochs,model,opt,scheduler,train_dl,train_size):
         loss4 = F.binary_cross_entropy_with_logits(sal_1_16, label) + iou_loss(sal_1_16, label)
         loss3 = F.binary_cross_entropy_with_logits(sal_1_8, label) + iou_loss(sal_1_8, label)
         loss2_ = F.binary_cross_entropy_with_logits(sal_1_4, label) + iou_loss(sal_1_4, label)
-        loss2 = structure_loss(mask_1_4, label, unc_1_4)#wbce(mask_1_4, label, unc_1_4) + iou_loss(mask_1_4, label)
-        loss1 = structure_loss(mask_1_2, label, unc_1_2)#wbce(mask_1_2, label, unc_1_2) + iou_loss(mask_1_2, label)
-        loss0 = structure_loss(mask_1_1, label, unc_1_1)#wbce(mask_1_1, label, unc_1_1) + iou_loss(mask_1_1, label)
-        #loss2 = F.binary_cross_entropy_with_logits(mask_1_4, label) + iou_loss(mask_1_4, label)
-        #loss1 = F.binary_cross_entropy_with_logits(mask_1_2, label) + iou_loss(mask_1_2, label)
-        #loss0 = F.binary_cross_entropy_with_logits(mask_1_1, label) + iou_loss(mask_1_1, label)
+        #loss2 = structure_loss(mask_1_4, label, unc_1_4)#wbce(mask_1_4, label, unc_1_4) + iou_loss(mask_1_4, label)
+        #loss1 = structure_loss(mask_1_2, label, unc_1_2)#wbce(mask_1_2, label, unc_1_2) + iou_loss(mask_1_2, label)
+        #loss0 = structure_loss(mask_1_1, label, unc_1_1)#wbce(mask_1_1, label, unc_1_1) + iou_loss(mask_1_1, label)
+        loss2 = F.binary_cross_entropy_with_logits(mask_1_4, label) + iou_loss(mask_1_4, label)
+        loss1 = F.binary_cross_entropy_with_logits(mask_1_2, label) + iou_loss(mask_1_2, label)
+        loss0 = F.binary_cross_entropy_with_logits(mask_1_1, label) + iou_loss(mask_1_1, label)
         
         loss = loss_weights[0] * loss0 + loss_weights[1] * loss1 + loss_weights[2] * loss2 + loss_weights[2] * loss2_ + loss_weights[3] * loss3 + loss_weights[4] * loss4
 
-        scloss3 = consistent_loss(sal_1_8,sal_1_16.detach()) * 0.0001
-        scloss2 = consistent_loss(sal_1_4,sal_1_8.detach()) * 0.0001
+        #scloss3 = consistent_loss(sal_1_8,sal_1_16.detach()) * 0.0001
+        #scloss2 = consistent_loss(sal_1_4,sal_1_8.detach()) * 0.0001
 
-        loss = loss + scloss3 + scloss2
+        loss = loss# + scloss3 + scloss2
 
         opt.zero_grad()
         loss.backward()
